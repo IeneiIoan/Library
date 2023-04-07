fe8e2a267c56fca98c1bcc89122dd5090bf0274f
Iienei <78168762+Iienei@users.noreply.github.com>
2023-04-07 19:05:03 +0300

    add form in html, and add form functionality in JS

--- script.js
+++ script.js
@@ -10,13 +10,50 @@ function Book(title, author, pages, read) {
     }
 }
 
-const theHobbit = new Book ('The Hobbit', 'J.R.R. Tolkien', '295', 'not read yet');
+let button = document.getElementById("button");
+const container = document.querySelector('.container');
 
-console.log(theHobbit.info());
+button.addEventListener("click", (e) => {
+    e.preventDefault();
+    container.textContent = "";
+    createBook();
+    let form = document.querySelector("form");
+    form.reset();
+});
 
-function addBookToLibrary() {
-    
-    console.log(myLibrary);
+function createBook() {
+    let {title, author, pages, readStatus} = getFormData();
+    const book = new Book(title, author, pages, readStatus);
+    addBookToLibrary(book);
 }
 
-addBookToLibrary();
+function getFormData() {
+    let title = document.getElementById("title").value;
+    let author = document.getElementById("author").value;
+    let pages = document.getElementById("pages").value;
+    let readStatus = document.getElementById("readStatus").checked;
+
+    return {title, author, pages, readStatus};
+}
+
+function displayBook(title, author, pages, readStatus) {
+    const content = document.createElement('div');
+    content.classList.add('content');
+    let p1 = document.createElement('p');
+    p1.textContent = title;
+    let p2 = document.createElement('p');
+    p2.textContent = author;
+    let p3 = document.createElement('p');
+    p3.textContent = pages;
+    let p4 = document.createElement('p');
+    p4.textContent = readStatus;
+
+    content.append(p1, p2, p3, p4);
+    container.appendChild(content);
+}
+
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+    myLibrary.forEach((book) =>  displayBook(book.title, book.author, book.pages, book.read));
+    console.log(myLibrary);
+}
